d2592ce59cb13493666ebb2c85501f867d9e5267
sasha-sup <a.suprun1312@gmail.com>
2023-12-18 18:38:45 +0400

    fix

--- bot/bot.py
+++ bot/bot.py
@@ -49,30 +49,30 @@ async def get_random_file():
         logger.error(f"Error get random file: {e}")
 
 # notifier
-async def send_messages(bot):
-    try:
-        while True:
-            users = await db.get_all_user_ids()
-            interval = timedelta(hours=(random.randint(MIN_TIME, MAX_TIME)))
-            for user_id in users:
-                try:
-                    path = await get_random_file()
-                    if "video" in path:
-                        video = FSInputFile(path)
-                        await bot.send_video(user_id, video=video)
-                    elif "pic" in path:
-                        pic = FSInputFile(path)
-                        await bot.send_photo(user_id, photo=pic)
-                except Exception as e:
-                    logger.error(f"Error send_random_file: {e}")
-                    logger.warning(f"User {user_id} has blocked the bot. Skipping.")
-                logger.info(f"Sent a message to {user_id}")
-                await asyncio.sleep(10) # delay in seconds
-            logger.info(f"Next iteration in: {interval} hours")
-            await asyncio.sleep(interval.total_seconds())
-    except Exception as e:
-        await bot.reply("⚠️ Something went wrong. Try again or contact admin.")
-        logger.error(f"Error send_messages: {e}")
+# async def send_messages(bot):
+#     try:
+#         while True:
+#             users = await db.get_all_user_ids()
+#             interval = timedelta(hours=(random.randint(MIN_TIME, MAX_TIME)))
+#             for user_id in users:
+#                 try:
+#                     path = await get_random_file()
+#                     if "video" in path:
+#                         video = FSInputFile(path)
+#                         await bot.send_video(user_id, video=video)
+#                     elif "pic" in path:
+#                         pic = FSInputFile(path)
+#                         await bot.send_photo(user_id, photo=pic)
+#                 except Exception as e:
+#                     logger.error(f"Error send_random_file: {e}")
+#                     logger.warning(f"User {user_id} has blocked the bot. Skipping.")
+#                 logger.info(f"Sent a message to {user_id}")
+#                 await asyncio.sleep(10) # delay in seconds
+#             logger.info(f"Next iteration in: {interval} hours")
+#             await asyncio.sleep(interval.total_seconds())
+#     except Exception as e:
+#         await bot.reply("⚠️ Something went wrong. Try again or contact admin.")
+#         logger.error(f"Error send_messages: {e}")
 
 # /bulk
 @dp.message(Command("bulk"))
@@ -101,9 +101,9 @@ async def main():
     await db.create_tables_if_exists()
     dp.include_routers(handlers.router)
     await bot.delete_webhook(drop_pending_updates=True)
-    #send_messages_task = asyncio.create_task(send_messages(bot))
+    send_messages_task = asyncio.create_task(send_messages(bot))
     await dp.start_polling(bot)
-    #await asyncio.gather(send_messages_task)
+    await asyncio.gather(send_messages_task)
 
 if __name__ == "__main__":
     asyncio.run(main())
