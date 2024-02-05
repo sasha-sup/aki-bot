37ccfbde6c0687ed2ea28dd0688a15e11954f8eb
Your Name <you@example.com>
2024-02-05 10:24:04 +0400

    feat: log parse for bad vid files

---
+++ logo-4tec/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+    source .env
+fi
+
+log_file="/var/log/akibot/main.log"
+message=""
+
+error_lines=$(grep "Error with .*: HTTP Client says - Request Entity Too Large" "$log_file" | awk -F 'Error with |: HTTP Client says - Request Entity Too Large' '{print $2}')
+
+send_telegram_message() {
+    local message="$1"
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+        -d "chat_id=$CHAT_ID" \
+        -d "text=$message"
+}
+
+if [ -n "$error_lines" ]; then
+    message+="🎥 Bad video:
+$error_lines"
+    send_telegram_message "$message"
+fi
+
+
